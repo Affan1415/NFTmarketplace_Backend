f74be5b03c5e60472606162d09ad8a2a83e250ad
Mahihecker <129369737+Mahihecker@users.noreply.github.com>
2024-04-24 00:11:53 +0500

    MVC

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const nftsRouter = require("./routes/nftsRoute");
+const usersRouter = require("./routes/usersRoute");
+
+const app = express();
+app.use(express.json());
+
+// if (process.env.NODE_ENV === "development ") {
+//   app.use(morgan("dev"));
+// }
+app.use(morgan("dev"));
+// //SERVING TEMPLATE DEMO
+// app.use(express.static(`${__dirname}/nft-data/img`));
+
+//CUSTOM MIDDLE WARE
+app.use((req, res, next) => {
+  console.log("Hey i am from middleware function 👋");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use("/api/v1/nfts", nftsRouter);
+app.use("/api/v1/users", usersRouter);
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
 // const fs = require("fs");//>>>this fs model come along with express (using it to read/write the data from the data folder)
 // const express = require("express");
 
@@ -258,187 +298,13 @@
 
 // /////PART 3 -----------------------
 
-const fs = require("fs");
-const express = require("express");
-const morgan = require("morgan");
-
-const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
-
-//CUSTOM MIDDLE WARE
-app.use((req, res, next) => {
-  console.log("Hey i am from middleware function 👋");
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
-const nfts = JSON.parse(
-  fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
-);
-//GET METHOD
-const getAllNfts = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: "success",
-    requestTime: req.requestTime,
-    results: nfts.length,
-    data: {
-      nfts,
-    },
-  });
-};
-// //POST METHOD
-const createNFT = (req, res) => {
-  const newId = nfts[nfts.length - 1].id + 1;
-  const newNFTs = Object.assign({ id: newId }, req.body);
-
-  nfts.push(newNFTs);
-
-  fs.writeFile(
-    `${__dirname}/nft-data/data/nft-simple.json`,
-    JSON.stringify(nfts),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        nft: newNFTs,
-      });
-    }
-  );
-};
-// // GET SINGLE NFT
-const getSingleNFT = (req, res) => {
-  const id = req.params.id * 1;
-  const nft = nfts.find((el) => el.id === id);
-
-  if (!nft) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      nft,
-    },
-  });
-};
-// //PATCH METHOD
-const updateNFT = (req, res) => {
-  if (req.params.id * 1 > nfts.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      nft: "Updating nft",
-    },
-  });
-};
-// //DELET METHOD
-const deleteNFT = (req, res) => {
-  if (req.params.id * 1 > nfts.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-};
-
-// ///------USERS
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
-
-const getSingleUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-};
-//>>splitting the code 
-//>>if any request made it will first hit these routers and thenhit the otehr routers
-const nftsRouter = express.Router();
-const usersRouter = express.Router();
-
-//ROUTER NFTs
-nftsRouter.route("/").get(getAllNfts).post(createNFT);
-
-nftsRouter.route("/:id").get(getSingleNFT).patch(updateNFT).delete(deleteNFT);
-
-//ROUTERS USERS
-usersRouter.route("/").get(getAllUsers).post(createUser);
-
-usersRouter
-  .route("/:id")
-  .get(getSingleUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
-app.use("/api/v1/nfts", nftsRouter);
-app.use("/api/v1/users", usersRouter);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
-});
-
-/////PART 4 -----------------------
-
+// const fs = require("fs");
 // const express = require("express");
 // const morgan = require("morgan");
 
-// const nftsRouter = require("./routes/nftsRoute");
-// const usersRouter = require("./routes/usersRoute");
-
 // const app = express();
 // app.use(express.json());
-
-// if (process.env.NODE_ENV === "development ") {
-//   app.use(morgan("dev"));
-// }
 // app.use(morgan("dev"));
-// //SERVING TEMPLATE DEMO
-// app.use(express.static(`${__dirname}/nft-data/img`));
 
 // //CUSTOM MIDDLE WARE
 // app.use((req, res, next) => {
@@ -451,7 +317,151 @@ app.listen(port, () => {
 //   next();
 // });
 
+// const nfts = JSON.parse(
+//   fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
+// );
+// //GET METHOD
+// const getAllNfts = (req, res) => {
+//   console.log(req.requestTime);
+//   res.status(200).json({
+//     status: "success",
+//     requestTime: req.requestTime,
+//     results: nfts.length,
+//     data: {
+//       nfts,
+//     },
+//   });
+// };
+// // //POST METHOD
+// const createNFT = (req, res) => {
+//   const newId = nfts[nfts.length - 1].id + 1;
+//   const newNFTs = Object.assign({ id: newId }, req.body);
+
+//   nfts.push(newNFTs);
+
+//   fs.writeFile(
+//     `${__dirname}/nft-data/data/nft-simple.json`,
+//     JSON.stringify(nfts),
+//     (err) => {
+//       res.status(201).json({
+//         status: "success",
+//         nft: newNFTs,
+//       });
+//     }
+//   );
+// };
+// // // GET SINGLE NFT
+// const getSingleNFT = (req, res) => {
+//   const id = req.params.id * 1;
+//   const nft = nfts.find((el) => el.id === id);
+
+//   if (!nft) {
+//     return res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: "success",
+//     data: {
+//       nft,
+//     },
+//   });
+// };
+// // //PATCH METHOD
+// const updateNFT = (req, res) => {
+//   if (req.params.id * 1 > nfts.length) {
+//     return res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: "success",
+//     data: {
+//       nft: "Updating nft",
+//     },
+//   });
+// };
+// // //DELET METHOD
+// const deleteNFT = (req, res) => {
+//   if (req.params.id * 1 > nfts.length) {
+//     return res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//     });
+//   }
+
+//   res.status(204).json({
+//     status: "success",
+//     data: null,
+//   });
+// };
+
+// // ///------USERS
+// const getAllUsers = (req, res) => {
+//   res.status(500).json({
+//     status: "error",
+//     message: "Internal server error",
+//   });
+// };
+
+// const createUser = (req, res) => {
+//   res.status(500).json({
+//     status: "error",
+//     message: "Internal server error",
+//   });
+// };
+
+// const getSingleUser = (req, res) => {
+//   res.status(500).json({
+//     status: "error",
+//     message: "Internal server error",
+//   });
+// };
+
+// const updateUser = (req, res) => {
+//   res.status(500).json({
+//     status: "error",
+//     message: "Internal server error",
+//   });
+// };
+
+// const deleteUser = (req, res) => {
+//   res.status(500).json({
+//     status: "error",
+//     message: "Internal server error",
+//   });
+// };
+// //>>splitting the code 
+// //>>if any request made it will first hit these routers and thenhit the otehr routers
+// const nftsRouter = express.Router();
+// const usersRouter = express.Router();
+
+// //ROUTER NFTs
+// nftsRouter.route("/").get(getAllNfts).post(createNFT);
+
+// nftsRouter.route("/:id").get(getSingleNFT).patch(updateNFT).delete(deleteNFT);
+
+// //ROUTERS USERS
+// usersRouter.route("/").get(getAllUsers).post(createUser);
+
+// usersRouter
+//   .route("/:id")
+//   .get(getSingleUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
+
 // app.use("/api/v1/nfts", nftsRouter);
 // app.use("/api/v1/users", usersRouter);
 
-// module.exports = app;
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}....`);
+// });
+
+
+
+
--- routes/nftsRoute.js
+++ routes/nftsRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nftControllers = require("./../controllers/nftControllers");
+//>>can import it this way too
 // const {
 //   getAllNfts,
 //   createNFT,
@@ -9,30 +10,35 @@ const nftControllers = require("./../controllers/nftControllers");
 // } = require("./../controllers/nftControllers");
 
 const router = express.Router();
-// router.param("id", nftControllers.checkId);
-
+router.param("id", nftControllers.checkId);
+//>>so whenever we got the id its only work for nfts not for user as it is onli defined in nftrouter
+// router.param("id",(req,res,next,value)=>{
+//   console.log(`ID: ${value}`);
+//   next();
+// })
 //TOP 5 NFTs BY PRICE
-router
-  .route("/top-5-nfts")
-  .get(nftControllers.aliasTopNFTs, nftControllers.getAllNfts);
+// router
+//   .route("/top-5-nfts")
+//   .get(nftControllers.aliasTopNFTs, nftControllers.getAllNfts);
 
-//STATS ROUTE
-router.route("/nfts-stats").get(nftControllers.getNFTsStats);
+// //STATS ROUTE
+// router.route("/nfts-stats").get(nftControllers.getNFTsStats);
 
-//GET MONTHLY PLAN
-router.route("/monthly-plan/:year").get(nftControllers.getMonthlyPlan);
+// //GET MONTHLY PLAN
+// router.route("/monthly-plan/:year").get(nftControllers.getMonthlyPlan);
 
-//ROUTER NFTs
+// //ROUTER NFTs
 router
   .route("/")
   .get(nftControllers.getAllNfts)
-  // .post(nftControllers.checkBody, nftControllers.createNFT);
-  .post(nftControllers.createNFT);
+  //>>we have to check if user providing all data before creating nft
+  .post(nftControllers.checkBody, nftControllers.createNFT);
+  //.post(nftControllers.createNFT);
 
 router
   .route("/:id")
   .get(nftControllers.getSingleNFT)
   .patch(nftControllers.updateNFT)
   .delete(nftControllers.deleteNFT);
-
+//>>export route so we can recieve it in app.js
 module.exports = router;
