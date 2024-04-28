e962d4aa9cf2e7f5fe0f22f11cdd33e9ef1059ef
Mahihecker <129369737+Mahihecker@users.noreply.github.com>
2024-04-28 19:25:40 +0500

    refactoring

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError =require("./Utils/appError");
+const globalErrorHandler= require("./controllers/errorController");
 const nftsRouter = require("./routes/nftsRoute");
 const usersRouter = require("./routes/usersRoute");
 
@@ -31,13 +33,30 @@ app.use("/api/v1/users", usersRouter);
 
 //>>non of the middleware above catch the request->Error middlesware
 //>>.all ->trigger for all the eror (*)means global
-app.all("*",(req,res)=> {
-  res.status(404).json({
-    status: "fail",
-    message:`Can't find ${req.originalUrl} on this server`,
-  });
-});
+app.all("*",(req,res,next)=> {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message:`Can't find ${req.originalUrl} on this server`,
+  // });
+  // const err=new Error(`Can't find ${req.originalUrl} on this server`)
+  // err.status="fail";
+  // err.statusCode=404
+  // next(err);
 
+  next(new AppError(`Can't find ${req.originalUrl} on this server`,404));
+});
+//global error handling
+app.use(globalErrorHandler);
+// app.use((err,req,res,next)=>{
+//   err.statusCode= err.statusCode || 500;
+//   err.status=err.status || "error";
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+
+// });
+// next();
+// });
 module.exports = app;
 
 
