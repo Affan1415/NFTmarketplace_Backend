cbdac69648d6af8c1bcc6ba6b2ca779a3daedc10
Mahihecker <129369737+Mahihecker@users.noreply.github.com>
2024-05-01 21:01:55 +0500

    production erroes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,67 @@
+const AppError=require("../Utils/appError");
+const handleCastErrorDB=err=>{
+  const message=`Invalid ${err.path}:${err.value}`;
+  //>>400 wrong data
+  return new AppError(message,400);
+
+};
+const handleDuplicateFieldsBD=(err)=>{
+  //>>/(?<=")(?:\\.|[^"\\])*(?=")/ to access the object
+  const value=err.errmsg.match(/(?<=")(?:\\.|[^"\\])*(?=")/);
+  //${value} to access name of the object
+
+  const message =`Duplicate field values ${value}.Please use another value`;
+  return new AppError(message,400);
+
+};
+const handleValidationError=(err)=>{
+  const errors=Object.values(err.errors).map(el=>el.message);
+  const message=`Invalid input Data. ${errors.join(". ")}`;
+  return new AppError(message,400);
+
+};
+const sendErrorDev=(err,res)=>{
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error:err,
+    stack:err.stack,
+
+});
+
+};
+const sendErrorPro=(err,res)=>{
+  if(err.isOperational){
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+
+});
+  }else{
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong",
+    });
+  }
+
+};
+
 module.exports=(err,req,res,next)=>{
     err.statusCode= err.statusCode || 500;
     err.status=err.status || "error";
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-  
-  });
+
+    if(process.env.NODE_ENV === "development"){
+      sendErrorDev(err,res);
+
+    }else if (process.env.NODE_ENV ==="production"){
+      let error={...err};
+      if(error.name==="CastError") error=handleCastErrorDB(error);
+      if(error.code===11000) error=handleDuplicateFieldsBD(error);
+      if(error.name==="ValidationError")error=handleValidationError(error);
+
+
+      sendErrorPro(error,res);
+    }
+    
   next();
 };
