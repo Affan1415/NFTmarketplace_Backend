b957e2a25ed223c926531926993bdf210dacd448
menalNaeem <153226991+menalNaeem@users.noreply.github.com>
2024-05-08 01:02:58 +0500

    error production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -30,3 +30,20 @@ const sendErrorDev=(err,res)=>{
 });
 
 };
+const sendErrorPro=(err,res)=>{
+  if(err.isOperational){
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+
+});
+  }else{
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong",
+    });
+  }
+
+};
+
