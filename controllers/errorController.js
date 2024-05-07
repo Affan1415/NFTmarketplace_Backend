ce04f8c39cfa081995096e17931187a9ebbc0027
menalNaeem <153226991+menalNaeem@users.noreply.github.com>
2024-05-08 01:01:24 +0500

    handel validation error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,3 +14,9 @@ const handleDuplicateFieldsBD=(err)=>{
   return new AppError(message,400);
 
 };
+const handleValidationError=(err)=>{
+  const errors=Object.values(err.errors).map(el=>el.message);
+  const message=`Invalid input Data. ${errors.join(". ")}`;
+  return new AppError(message,400);
+
+};
